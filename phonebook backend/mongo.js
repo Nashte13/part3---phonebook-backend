6c40fdbf381b4ae71dd9a5bc0f33922c60460416
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-01-30 17:21:27 +0300

    initial mongo DB test

---
+++ phonebook backend/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+    console.log('give a password as an argument')
+        process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://nash:${password}@cluster0.6xgmy0j.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, {family: 4})
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = Note({
+    content: 'Html is easy',
+    important: true,
+})
+
+note.save().then(result => {
+    console.log('notesaved')
+    mongoose.connection.close()
+})
