f9d739dc8a6ce0882dd8c76b31ee6cb991a912f6
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-06 11:53:41 +0300

    importing dotenv and setting port

--- phonebook backend/index.js
+++ phonebook backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -121,13 +122,7 @@ app.delete('/api/persons/:id', (req, res) => {
     }
 })
 
-
-
-
-
-
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
