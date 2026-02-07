5fe4165df1330ebcd32dd117766a77acaced48b9
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-07 20:17:43 +0300

    correcting error catching in the right promise

--- phonebook backend/index.js
+++ phonebook backend/index.js
@@ -78,9 +78,11 @@ app.post("/api/persons", (req, res, next) => {
 
       person.save().then((savedPerson) => {
         res.json(savedPerson);
-      });
+      })
+      .catch((error) => next(error));
     })
     .catch((error) => next(error));
+    
 });
 
 //update a person number
@@ -137,9 +139,9 @@ const unknownEndpoint = (req, res) => {
 app.use(unknownEndpoint);
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error.message);
+  console.error(error.message, error.name);
 
-  if (error.name === "castError") {
+  if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
   }else if (error.name === 'ValidationError') {
     return res.status(400).json({error: error.message})
