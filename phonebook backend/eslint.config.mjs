031ae211c9ff6cad7a12091cb59f53bb8e42d5ea
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-09 14:27:09 +0300

    correcting ecmaVersion syntax

--- phonebook backend/eslint.config.mjs
+++ phonebook backend/eslint.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig([
     languageOptions: { 
       sourceType: "commonjs",
       globals: {...globals.node},
-      ecmaVersions: 'latest',
+      ecmaVersion: 'latest',
     } 
   },
 
