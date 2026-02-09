0d0486a27484b8ccc8e91f4227283756ac2cfb14
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-09 14:21:41 +0300

    declaring plugin to file

--- phonebook backend/eslint.config.mjs
+++ phonebook backend/eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin'
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -12,4 +13,16 @@ export default defineConfig([
       ecmaVersions: 'latest',
     } 
   },
+
+  {
+    plugins: { 
+      '@stylistic/js': stylisticJs,
+    },
+    rules: { 
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    }, 
+  }
 ]);
