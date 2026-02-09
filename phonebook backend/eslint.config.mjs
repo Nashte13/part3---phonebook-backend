57050868a7cc03c92fd2294bacfb2c8b0a29fe6b
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-09 14:28:59 +0300

    formatting code

--- phonebook backend/eslint.config.mjs
+++ phonebook backend/eslint.config.mjs
@@ -1,32 +1,36 @@
 import globals from "globals";
-import js from '@eslint/js'
-import stylisticJs from '@stylistic/eslint-plugin'
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   js.configs.recommended,
-  { 
-    files: ["**/*.js"], 
-    languageOptions: { 
+  {
+    files: ["**/*.js"],
+    languageOptions: {
       sourceType: "commonjs",
-      globals: {...globals.node},
-      ecmaVersion: 'latest',
-    } 
+      globals: { ...globals.node },
+      ecmaVersion: "latest",
+    },
   },
 
   {
-    plugins: { 
-      '@stylistic/js': stylisticJs,
+    plugins: {
+      "@stylistic/js": stylisticJs,
     },
-    rules: { 
-      '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
-    }, 
+    rules: {
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/linebreak-style": ["error", "unix"],
+      "@stylistic/js/quotes": ["error", "single"],
+      "@stylistic/js/semi": ["error", "never"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+    },
   },
 
-  { 
-    ignores: ['dist/**'], 
+  {
+    ignores: ["dist/**"],
   },
 ]);
