6a6f053e1cd373613597d9715d74d9a81a8e50be
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-09 14:12:00 +0300

    configuring file

--- phonebook backend/eslint.config.mjs
+++ phonebook backend/eslint.config.mjs
@@ -2,6 +2,12 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { 
+      sourceType: "commonjs",
+      globals: {...globals.node},
+      ecmaVersions: 'latest',
+    } 
+  },
 ]);
