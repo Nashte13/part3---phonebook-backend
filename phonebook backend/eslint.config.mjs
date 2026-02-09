62234d1b583519e5171ecc55af1a4ab59761c427
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-09 14:30:29 +0300

    disabling no console rule

--- phonebook backend/eslint.config.mjs
+++ phonebook backend/eslint.config.mjs
@@ -27,6 +27,7 @@ export default defineConfig([
       "no-trailing-spaces": "error",
       "object-curly-spacing": ["error", "always"],
       "arrow-spacing": ["error", { before: true, after: true }],
+      'no-console': 'off',
     },
   },
 
